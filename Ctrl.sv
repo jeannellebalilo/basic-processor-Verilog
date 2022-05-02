f839fc5f896ab0d7d4667912f370db200c9d47c0
jeannellebalilo <91799468+jeannellebalilo@users.noreply.github.com>
2022-05-02 16:03:30 -0700

    Edited CTRL block

--- Ctrl.sv
+++ Ctrl.sv
@@ -13,11 +13,10 @@ import Definitions::*;
 module Ctrl (
   input  [8:0] Instruction,    // machine code
                                // some designs use ALU inputs here
-  output logic       Jump,
-                     BranchEn, // branch at all?
+  output logic       BranchEn, // branch at all?
                      RegWrEn,  // write to reg_file (common)
                      MemWrEn,  // write to mem (store only)
-                     LoadInst, // mem or ALU to reg_file ?
+                     ALUEn,    // using ALU
                      Ack,      // "done with program"
   output logic [1:0] TargSel   // how to target branch (maybe?)
 );
@@ -27,12 +26,26 @@ module Ctrl (
 //
 // Note: This **starter code** is not a complete ISA!
 
+/*
+OPCODES
+lw -  0000
+lwl - 0001
+sw -  0010
+swl - 0011
+xor - 0100
+add - 0101
+lsr - 0110
+lsl - 0111
+mov - 1000
+sne - 1001
+seq - 1010
+boo - 1011
+lut - 1100
+bol - 1101
+msk - 1110
+*/
 
-// instruction = 9'b110??????;
-assign MemWrEn = Instruction[8:6] == 3'b110;
-
-assign RegWrEn = Instruction[8:7] != 2'b11;
-assign LoadInst = Instruction[8:6] == 3'b011;
+assign Opcode = Instruction[8:5];
 
 // reserve instruction = 9'b111111111; for Ack
 assign Ack = &Instruction;
@@ -40,11 +53,39 @@ assign Ack = &Instruction;
 // jump on right shift that generates a zero
 // equiv to simply: assign Jump = Instruction[2:0] == RSH;
 always_comb begin
-  if(Instruction[2:0] == RSH) begin
-    Jump = 1;
-  end else begin
-    Jump = 0;
-  end
+  case (Opcode)
+    0000: RegWrEn = 1;
+
+    0001: RegWrEn = 1;
+
+    0010: MemWrEn = 1;
+
+    0011: MemWrEn = 1;
+
+    0100: ALUEn = 1;
+
+    0101: ALUEn = 1;
+
+    0110: ALUEn = 1;
+
+    0111: ALUEn = 1;
+
+    1000: 
+
+    1001: ALUEn = 1;
+
+    1010: ALUEn = 1;
+
+    1011: BranchEn = 1;
+
+    1100:
+
+    1101: BranchEn = 1;
+
+    1110: ALUEn = 1;
+
+    default: 
+  endcase
 end
 
 // branch every time instruction = 9'b?????1111;
