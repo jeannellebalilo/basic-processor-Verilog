cae7c3c40b33c85fa7d45f80c4e789aaf5d50c4d
Jeannelle Balilo <jeannellebalilo@gmail.com>
2022-05-09 17:01:56 -0700

    new LUT for DataMem, edited LUT for PC

--- LUT_dm.sv
+++ LUT_dm.sv
@@ -1,7 +1,7 @@
 // Design Name:    CSE141L
-// Module Name:    LUT
+// Module Name:    LUT_dm.sv
 
-// possible lookup table for PC target
+// possible lookup table for DataMem target
 // leverage a few-bit pointer to a wider number
 // Lookup table acts like a function: here Target = f(Addr);
 // in general, Output = f(Input)
@@ -17,11 +17,11 @@ module LUT(
 always_comb begin
 
   case(Addr)
-    // TODO: figure out the PC addresses we need!
-    2'b00: Target = 10'h3f0; // -16, i.e., move back 16 lines of machine code
-    2'b01: Target = 10'h003;
-    2'b10: Target = 10'h007;
-    2'b11: Target = 10'h001; // default to 1 (or PC+1 for relative)
+    // TODO: figure out the DataMem addresses we need!
+    2'b00: Target = 8'b0;
+    2'b01: Target = 8'b0;
+    2'b10: Target = 8'b0;
+    2'b11: Target = 8'b0;
   endcase
 end
 
