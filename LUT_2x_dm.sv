457a3108d983d4d6444dd1d7cf04da65cddf187f
IvanRobles <ivanroblesmvp@gmail.com>
2022-05-22 00:43:52 -0700

    2x_dm should work, pc missing #2 and #4 IM address

--- LUT_2x_dm.sv
+++ LUT_2x_dm.sv
@@ -25,43 +25,43 @@ always_comb begin
       case(EntryReg)
         // These are the DataMem addresses we want to save variables that require
         // more information to map (such as parity bit combinations)
-        8'b00000000: Target = 10'd72;
-        8'b00000001: Target = 10'd73;
-        8'b00000010: Target = 10'd74;
-        8'b00000011: Target = 10'd75;
-        8'b00000100: Target = 10'd76;
-        8'b00000101: Target = 10'd77;
-        8'b00000110: Target = 10'd78;
-        8'b00000111: Target = 10'd79;
-        8'b00001000: Target = 10'd80;
-        8'b00001001: Target = 10'd81;
-        8'b00001010: Target = 10'd82;
-        8'b00001011: Target = 10'd83;
-        8'b00001100: Target = 10'd84;
-        8'b00001101: Target = 10'd85;
-        8'b00001110: Target = 10'd86;
-        8'b00001111: Target = 10'd87;
+        8'b00000000: Target = 10'd0; //The case where all are correct
+        8'b00000001: Target = 10'd13; //The rest are the case we can't correct the bits
+        8'b00000010: Target = 10'd13;
+        8'b00000011: Target = 10'd13;
+        8'b00000100: Target = 10'd13;
+        8'b00000101: Target = 10'd13;
+        8'b00000110: Target = 10'd13;
+        8'b00000111: Target = 10'd13;
+        8'b00001000: Target = 10'd13;
+        8'b00001001: Target = 10'd13;
+        8'b00001010: Target = 10'd13;
+        8'b00001011: Target = 10'd13;
+        8'b00001100: Target = 10'd13;
+        8'b00001101: Target = 10'd13;
+        8'b00001110: Target = 10'd13;
+        8'b00001111: Target = 10'd13;
       endcase
     end
     // The MuxReg is 1
     8'b00000001: begin
       case(EntryReg)
-        8'b00000000: Target = 10'd88;
-        8'b00000001: Target = 10'd89;
-        8'b00000010: Target = 10'd90;
-        8'b00000011: Target = 10'd91;
-        8'b00000100: Target = 10'd92;
-        8'b00000101: Target = 10'd93;
-        8'b00000110: Target = 10'd94;
-        8'b00000111: Target = 10'd95;
-        8'b00001000: Target = 10'd96;
-        8'b00001001: Target = 10'd97;
-        8'b00001010: Target = 10'd98;
-        8'b00001011: Target = 10'd99;
-        8'b00001100: Target = 10'd100;
-        8'b00001101: Target = 10'd101;
-        8'b00001110: Target = 10'd102;
-        8'b00001111: Target = 10'd103;
+        8'b00000000: Target = 10'd12; //The case where there is a parity error only
+        8'b00000001: Target = 10'd12; //The case where there is a parity error only
+        8'b00000010: Target = 10'd12; //The case where there is a parity error only
+        8'b00000011: Target = 10'd1; //The case where bit 1 is wrong
+        8'b00000100: Target = 10'd12; //The case where there is a parity error only
+        8'b00000101: Target = 10'd2; //The case where bit 2 is wrong
+        8'b00000110: Target = 10'd3; //The case where bit 3 is wrong
+        8'b00000111: Target = 10'd4; //The case where bit 4 is wrong
+        8'b00001000: Target = 10'd12; //The case where there is a parity error only
+        8'b00001001: Target = 10'd5; //The case where bit 5 is wrong
+        8'b00001010: Target = 10'd6; //The case where bit 6 is wrong
+        8'b00001011: Target = 10'd7; //The case where bit 7 is wrong
+        8'b00001100: Target = 10'd8; //The case where bit 8 is wrong
+        8'b00001101: Target = 10'd9; //The case where bit 9 is wrong
+        8'b00001110: Target = 10'd10; //The case where bit 10 is wrong
+        8'b00001111: Target = 10'd11; //The case where bit 11 is wrong
       endcase
     end
   endcase
