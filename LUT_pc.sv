c06c276052714a9e19f1770355d5d2f9d6bcb1dc
IvanRobles <ivanroblesmvp@gmail.com>
2022-06-07 15:27:48 -0700

    Added LUT jump addresses and text of assembly

--- LUT_pc.sv
+++ LUT_pc.sv
@@ -8,7 +8,7 @@
 
 module LUT_pc(
   input        [ 3:0] Addr,            //Immediate to determine entry
-  output logic [ 9:0] Target           //InstMem Address we want 
+  output logic [ 9:0] Target           //PC Address we want 
 );
 
 always_comb begin
@@ -18,9 +18,9 @@ always_comb begin
     // absolute jumps in Instruction Memory, not relative to curr PC
     4'b0000: Target = 10'd15; // #0 landing in the assembly (Program 1)
     4'b0001: Target = 10'd15; // #1 landing in the assembly (Program 2)
-    4'b0010: Target = 10'd; // #2 landing in the assembly (Program 3)
+    4'b0010: Target = 10'd238; // #2 landing in the assembly (Program 3)
     4'b0011: Target = 10'd19; // #3 landing in the assembly (Program 3)
-    4'b0100: Target = 10'd; // #4 landing in the assembly (Program 2)
+    4'b0100: Target = 10'd335; // #4 landing in the assembly (Program 2)
     4'b0101: Target = 10'h005; //The rest are unused in the three programs
     4'b0110: Target = 10'h006; 
     4'b0111: Target = 10'h007; 
